c9ebabb177c69c1bac4609169360164cdcd3c4ef
huangfuhui <huangfuhui@outlook.com>
2019-09-08 19:24:12 +0800

    init help command

---
+++ cmd/help.go
@@ -0,0 +1,72 @@
+// Copyright 2019 migrant
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+)
+
+// show the usage of migrant.
+func ShowUsage() string {
+	return `
+A migrant tool for MySQL written by golang.
+
+Usage:
+	command [options] [arguments]
+
+Available commands:
+	help		Displays help for a command.
+	migrate		Migrate the migration file.
+	make		Create a new migration file.
+	rollback	Rollback the last database migration.
+	reset		Rollback all database migrations.
+	fresh		Drop all tables and re-run all migrations.
+`
+}
+
+type HelpCmd struct {
+	Command *Cmd
+}
+
+func ExecHelp(command *Cmd) {
+	help := &HelpCmd{command}
+	if err := help.Exec(); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func (h *HelpCmd) Exec() (err error) {
+	args := h.Command.Args
+	if len(args) == 0 {
+		fmt.Println(ShowUsage())
+		return
+	} else if len(args) > 1 {
+		return errors.New("command 'help' just need one argument.")
+	} else {
+
+	}
+
+	return
+}
+
+func (h *HelpCmd) Usage() string {
+	return `
+Displays help for a command.
+
+Usage:
+	help [command]
+`
+}
